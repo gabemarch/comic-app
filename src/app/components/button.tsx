8e1558e320fbc5143aad0afd2a38f91210cdbf6b
Gabor <szilagyi_gabor@outlook.com>
2024-11-13 10:54:25 +0100

    page and button converted to client component

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+"use client";
+
+import React from "react";
 
 interface ButtonProps {
   buttonText: string;
-  onClick: () => void
+  onClick: () => void;
 }
 
 const Button = ({ buttonText, onClick }: ButtonProps) => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" onClick={onClick}>
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+      onClick={onClick}
+    >
       {buttonText}
     </button>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Button from "@/components/button";
 
 export default function Home() {
