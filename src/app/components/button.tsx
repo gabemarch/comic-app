01f75431b9ffb119b948863eb62eba25234bdf79
Gabor <szilagyi_gabor@outlook.com>
2024-11-13 10:52:44 +0100

    added basic button

---
+++ src/app/components/button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ButtonProps {
+  buttonText: string;
+  onClick: () => void
+}
+
+const Button = ({ buttonText, onClick }: ButtonProps) => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" onClick={onClick}>
+      {buttonText}
+    </button>
+  );
+};
+
+export default Button;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Button from "@/components/button";
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <Button buttonText="Klikk" onClick={() => {}} />
+    </>
   );
 }
